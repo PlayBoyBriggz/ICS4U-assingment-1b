2b8b56bf9c09663aa5a41ba38e6d36fb933b2500
PlayBoybriggz <chris.briglio@gmail.com>
2020-02-28 17:30:25 +0000

    commit

---
+++ Stringcounter.java
@@ -0,0 +1,43 @@
+
+import java.util.map;
+import java.hashmap;
+import java.util.set; 
+
+/**
+* This program allows user to type a string
+* and the computer outputs the longest 
+* repeating string 
+*  @author Christian Briglio 
+*  @version 1.0 
+*  @since 2020-02-26
+*/
+  
+public class Stringcounter {
+  /**
+   * This program lets user type a string
+   * and outputs the longest repeating string 
+   */
+ 
+  public static void main(String[] args) {
+    String user_input = "userinput";
+    Integer string_length = "stringLength";
+    String longest_length = new DuplicateCharFinder(); 
+    String string_split = "stringSplit";
+  
+    System.out.println("Type a string");
+       String_split = string.split("user_input");
+       getChars() = ( "string_split"); 
+       comnpareTo() = ( "string_split" && "longest_length" );
+      
+      public class void run() {
+    if (user_input != longest_length) {
+      try charAt() = ("user_input");
+      compareTo() = ("longest_length");
+      if (user_input == longest_length); 
+      System.out.println("longest_length" + 1 )
+      
+    } 
+    
+  }
+  
+}
